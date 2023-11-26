e70f1241f2f406003ae1ca13e54c65ed7fbebc38
yuris <yls4@cornell.edu>
2023-11-26 17:55:38 -0500

    complete phase 5

---
+++ frontend/src/store/errors.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+import { sessionErrorsReducer } from './session';
+
+export default combineReducers({
+  session: sessionErrorsReducer
+});
+
+
+const nullErrors = null;
+
+export const sessionErrorsReducer = (state = nullErrors, action) => {
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+    case CLEAR_SESSION_ERRORS:
+      return nullErrors;
+    default:
+      return state;
+  }
+};
