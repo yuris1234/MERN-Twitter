f89e8fd068a9027e8c117d54c620f0fc5e3600b6
yuris <yls4@cornell.edu>
2023-11-26 12:55:33 -0500

    complete express server security

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,44 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-// const indexRouter = require('./routes/index');
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
-app.use(logger('dev')); // log request components (URL/method) to terminal
-app.use(express.json()); // parse JSON request body
-app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
-app.use(cookieParser()); // parse cookies as an object on req.cookies
-// app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, React files
+  // will be served statically on the Express server.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
 
 // Attach Express routers
-// app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,5 @@
+
 module.exports = {
-    mongoURI: process.env.MONGO_URI
-  }
+  mongoURI: process.env.MONGO_URI,
+  isProduction: process.env.NODE_ENV === 'production'
+}
