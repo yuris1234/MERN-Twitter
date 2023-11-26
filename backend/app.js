b55571933fa3a3ad0e2d8c6b8ef64615d21c7ab7
yuris <yls4@cornell.edu>
2023-11-26 12:46:56 -0500

    complete express routing

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
-
 const express = require("express");
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/api/users'); // update the import file path
+// const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
 
 const app = express();
 
@@ -13,10 +12,11 @@ app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
-app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
+// app.use(express.static(path.join(__dirname, 'public'))); // serve the static files in the public folder
 
 // Attach Express routers
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter); // update the path
+// app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;
