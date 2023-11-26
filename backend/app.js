5c8c341d4e154c32594395b7c4822a5e107a6bb7
yuris <yls4@cornell.edu>
2023-11-26 12:58:45 -0500

    complete express error handling

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
+const debug = require('debug');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -42,4 +43,25 @@ app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
-module.exports = app;
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
+  
+  module.exports = app;
